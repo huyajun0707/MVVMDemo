aee79516ada3b47933a7852652644d1bfb92b0b8
finup <huyajun@renmaitech.com>
2019-10-11 21:05:10 +0800

    提交

---
+++ MyDemo/jsbridgedemo/src/main/java/com/hyj/demo/jsbridgedemo/JsBridgeActivity.java
@@ -0,0 +1,13 @@
+package com.hyj.demo.jsbridgedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JsBridgeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_js_bridge);
+    }
+}
--- MyDemo/jsbridgedemo/src/main/java/com/hyj/demo/jsbridgedemo/MainActivity.java
+++ MyDemo/jsbridgedemo/src/main/java/com/hyj/demo/jsbridgedemo/MainActivity.java
@@ -1,31 +1,136 @@
 package com.hyj.demo.jsbridgedemo;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.JsResult;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.github.lzyzsd.jsbridge.BridgeHandler;
 import com.github.lzyzsd.jsbridge.BridgeWebView;
 import com.github.lzyzsd.jsbridge.CallBackFunction;
 import com.github.lzyzsd.jsbridge.WebViewJavascriptBridge;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
-    private BridgeWebView webView;
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        webView = findViewById(R.id.webview);
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+
+        // 设置与Js交互的权限
+        webSettings.setJavaScriptEnabled(true);
+        // 设置允许JS弹窗
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.loadUrl("file:///android_asset/index.html");
+//        webView.loadUrl("https://cn.bing.com");
+        findViewById(R.id.btCallJs).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //方式一
+
+                // 注意调用的JS方法名要对应上
+                // 调用javascript的callJs()方法
+//                webView.loadUrl("javascript:callJs('方式一')");
+
+                //方式二
+                // 只需要将第一种方法的loadUrl()换成下面该方法即可
+                webView.evaluateJavascript("javascript:callJs('方式二')", new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String value) {
+                        //此处为 js 返回的结果
+                        Log.d("callJs返回值", "onReceiveValue: " + value);
+                    }
+                });
+            }
+
+
+        });
+
+
+        // 由于设置了弹窗检验调用结果,所以需要支持js对话框
+        // webview只是载体，内容的渲染需要使用webviewChromClient类去实现
+        // 通过设置WebChromeClient对象处理JavaScript的对话框
+        //设置响应js 的Alert()函数
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
+                AlertDialog.Builder aletDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                aletDialogBuilder.setTitle("Alert");
+                aletDialogBuilder.setMessage(message);
+                aletDialogBuilder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        result.confirm();
+                    }
+                });
+                aletDialogBuilder.setCancelable(false);
+                aletDialogBuilder.create().show();
+                return true;
+            }
+
+        });
+
+
+        //JS调native方法
+        webView.addJavascriptInterface(new NativeJsInterface(), "android");
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Uri uri = Uri.parse(url);
+                Log.d("--->", "shouldOverrideUrlLoading:"+url);
+                if (uri.getScheme().equals("js")) {
+                    String param = uri.getQueryParameter("param");
+                    try {
+                        JSONObject jsonObject = new JSONObject(param);
+                        String method = jsonObject.getString("method");
+                        switch (method){
+                            case "goActivity":
+                                startActivity(new Intent(MainActivity.this, JsBridgeActivity.class));
+                                break;
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+                } else
+                    view.loadUrl(url);
+
+                return true;
+
+            }
+        });
 
 
     }
-    private void JSMethod(){
-//        webView.registerHandler("submitFromWeb", new BridgeHandler(){
-//            @Override
-//            public void handler(String data, CallBackFunction function){
-//                function.onCallBack("submitFrom web exe, response data from java");
-//            }
-//        });
+
+    //本地方法
+    public class NativeJsInterface {
+        @JavascriptInterface
+        public void goActivity() {
+            startActivity(new Intent(MainActivity.this, JsBridgeActivity.class));
+        }
     }
+
 
 }
